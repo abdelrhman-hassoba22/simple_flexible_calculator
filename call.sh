6798192eed25b3203144a528755dfa7a62148a8e
abdelrhman-hassoba22 <abdelrhmanhassoba@gmail.com>
2025-04-15 12:03:35 +0200

    I improved the code to make it more efficient and simpler, and this modification was made in the reading command by adding the -e flag.

--- call.sh
+++ call.sh
@@ -3,10 +3,9 @@
 # this variable to quit.
 q=0
 while [ $q -eq 0 ]; do
-printf "To enter the operation correctly, first write the numbers you want along with the desired operators without any spaces\n between the numbers and operators. Do not end the operation line with an operator \n to ensure the program produces correct outputs. "
-echo "An example for clarification: 1+2+5+7+8"
+echo "Do not place any operation at the beginning or the end."
 # this is the main variable.
-read -p "Enter your operation here:" opr
+read -ep "Enter your operation here:" opr
 echo $opr |bc  2> /dev/null
 check=`echo $?`
 if [ "$check" -eq 0 ]; then
